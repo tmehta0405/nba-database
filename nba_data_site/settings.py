ba7cac0cbb35b8110dc7ee66a88c4c42e9175bbd
Tirth Mehta <tirth11804@gmail.com>
2025-11-16 13:17:57 -0500

    quick static directory fix

--- nba_data_site/settings.py
+++ nba_data_site/settings.py
@@ -122,7 +122,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'stats' / 'static',
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
